e0ad4cd3ba9336d5c1c4d606aff53532548ef468
Jopand <jopand47@gmail.com>
2021-08-24 21:51:27 +0200

    Added user input entities for use with User Mode. No sensor does anything, yet

--- components/nilan/number/__init__.py
+++ components/nilan/number/__init__.py
@@ -14,6 +14,9 @@ from esphome.const import (
 )
  
 CONF_USER_TIME_SET = "user_time_set"
+CONF_USER_VENT_SET = "user_vent_set"
+CONF_USER_TEMP_SET = "user_temp_set"
+CONF_USER_TEMP_OFFSET_SET = "user_temp_offset_set"
 
 nilan_ns = cg.esphome_ns.namespace('nilan')
 NilanNumber = nilan_ns.class_('NilanNumber', number.Number, cg.Component)
@@ -21,7 +24,10 @@ NilanNumber = nilan_ns.class_('NilanNumber', number.Number, cg.Component)
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(NilanNumber),
     cv.GenerateID(CONF_NILAN_ID): cv.use_id(Nilan),
-    cv.Optional(CONF_USER_TIME_SET): number.NUMBER_SCHEMA
+    cv.Optional(CONF_USER_TIME_SET): number.NUMBER_SCHEMA,
+    cv.Optional(CONF_USER_VENT_SET): number.NUMBER_SCHEMA,
+    cv.Optional(CONF_USER_TEMP_SET): number.NUMBER_SCHEMA,
+    cv.Optional(CONF_USER_TEMP_OFFSET_SET): number.NUMBER_SCHEMA,
 }).extend(cv.COMPONENT_SCHEMA)
  
 def to_code(config):
@@ -32,3 +38,15 @@ def to_code(config):
         conf = config[CONF_USER_TIME_SET]
         yield cg.register_component(var, conf)
         yield number.register_number(var, conf, min_value=0, max_value=480, step=1.0)
+    if CONF_USER_VENT_SET in config:
+        conf = config[CONF_USER_VENT_SET]
+        yield cg.register_component(var, conf)
+        yield number.register_number(var, conf, min_value=0, max_value=4, step=1.0)
+    if CONF_USER_TEMP_SET in config:
+        conf = config[CONF_USER_TEMP_SET]
+        yield cg.register_component(var, conf)
+        yield number.register_number(var, conf, min_value=0, max_value=30, step=1.0)
+    if CONF_USER_TEMP_OFFSET_SET in config:
+        conf = config[CONF_USER_TEMP_OFFSET_SET]
+        yield cg.register_component(var, conf)
+        yield number.register_number(var, conf, min_value=0, max_value=30, step=1.0)
