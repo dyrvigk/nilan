07c5d824b9279e87216c4de26d741a6142fb91b1
Jopand <jopand47@gmail.com>
2021-11-18 21:07:55 +0100

    Nilan is now a VERY simple component. Modbus has been stripped out, since that part will be handled by "modbus_controller". Removed binary_sensor and text_sensor (+1 squashed commits)
    Squashed commits:
    
    [46f588a] Nilan is now a VERY simple component. Modbus has been stripped out, since that part will be handled by "modbus_controller". Removed binary_sensor and text_sensor

--- components/nilan/climate/__init__.py
+++ components/nilan/climate/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, sensor, text_sensor, number
-
+from .. import Nilan, CONF_NILAN_ID
 from esphome.const import (
     CONF_ID
 )
@@ -11,29 +11,31 @@ CONF_CURRENT_TEMP = "current_temp_sensor_id"
 CONF_FAN_SPEED = "fan_speed_sensor_id"
 CONF_MODE = "mode_text_sensor_id"
 
-nilan_climate = cg.esphome_ns.namespace('nilan_climate')
-NilanClimate = nilan_climate.class_('NilanClimate', climate.Climate, cg.Component)
-
+nilan_ns = cg.esphome_ns.namespace('nilan')
+NilanClimate = nilan_ns.class_('NilanClimate', climate.Climate, cg.Component)
+ 
 CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(NilanClimate),
+    cv.GenerateID(CONF_NILAN_ID): cv.use_id(Nilan),
     cv.Required(CONF_TARGET_TEMP): cv.use_id(number.Number),
     cv.Required(CONF_CURRENT_TEMP): cv.use_id(sensor.Sensor),
     cv.Required(CONF_FAN_SPEED): cv.use_id(number.Number),
     cv.Required(CONF_MODE): cv.use_id(text_sensor.TextSensor)
 }).extend(cv.COMPONENT_SCHEMA)
  
-async def to_code(config):
+def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await climate.register_climate(var, config)
+    yield cg.register_component(var, config)
+    yield climate.register_climate(var, config)
 
-    sens_set_temp = await cg.get_variable(config[CONF_TARGET_TEMP])
+    sens_set_temp = yield cg.get_variable(config[CONF_TARGET_TEMP])
     cg.add(var.set_temp_setpoint_number(sens_set_temp))
 
-    sens_current_temp = await cg.get_variable(config[CONF_CURRENT_TEMP])
+    sens_current_temp = yield cg.get_variable(config[CONF_CURRENT_TEMP])
     cg.add(var.set_current_temp_sensor(sens_current_temp))
 
-    sens_fan_speed = await cg.get_variable(config[CONF_FAN_SPEED])
+    sens_fan_speed = yield cg.get_variable(config[CONF_FAN_SPEED])
     cg.add(var.set_fan_speed_number(sens_fan_speed))
 
-    sens_mode = await cg.get_variable(config[CONF_MODE])
+    sens_mode = yield cg.get_variable(config[CONF_MODE])
     cg.add(var.set_mode_sensor(sens_mode))
